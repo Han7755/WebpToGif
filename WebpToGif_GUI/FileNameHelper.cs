5e7c6bc719c72ca43183531fe34040dad774c76f
Han7755 <gksmfgyrud@naver.com>
2024-06-21 03:16:50 +0900

    파일명 중복 감지 추가

---
+++ WebpToGif_GUI/FileNameHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebpToGif_GUI
+{
+    public static class FileNameHelper
+    {
+        public static string GetUniqueFileName(string directory, string fileName)
+        {
+            string fileExtension = Path.GetExtension(fileName);
+            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
+            string uniqueFileName = fileName;
+            int counter = 1;
+
+            while (File.Exists(Path.Combine(directory, uniqueFileName)))
+            {
+                uniqueFileName = $"{fileNameWithoutExtension} ({counter}){fileExtension}";
+                counter++;
+            }
+
+            return uniqueFileName;
+        }
+    }
+}
